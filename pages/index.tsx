03cf85581ad0b81abe7cfdbdd7ef82f5084ceb92
Rebecca Peckman <rebeccapeckman@gmail.com>
2023-01-23 19:50:36 -0800

    updated placeholder Home page Props type and getServerSideProps to generate props based on session and token status and fetch user playlists if token is valid, render playlist names in client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { GetServerSideProps } from 'next';
 import { useSession, getSession, signIn, signOut } from 'next-auth/react';
-import { Session } from 'next-auth';
 
 export default function Home(props: Props) {
   const { data: session } = useSession();
@@ -12,6 +11,14 @@ export default function Home(props: Props) {
         <button onClick={() => signOut()}>Log Out</button>
         <p>Authenticated: {`${props.authenticated}`}</p>
         <p>Username: {`${session?.user?.name}`}</p>
+        <p>Access Token: {props.accessToken}</p>
+        <ul>
+          {
+            props.playlists?.map((playlist: any) => (
+              <li key={playlist.id}>{playlist.name}</li>
+            ))
+          }
+        </ul>
       </div>
     );
   } else {
@@ -26,20 +33,67 @@ export default function Home(props: Props) {
 };
 
 type Props = {
-  session?: Session;
   authenticated: boolean;
+  accessToken?: 'none' | 'expired' | 'valid';
+  playlists?: unknown[];
+  error?: unknown;
 };
 
 export const getServerSideProps: GetServerSideProps<Props> = async ({ req, res }) => {
 	const session = await getSession({ req });
+  console.log(`\nINSIDE getServerSideProps, `);
+  console.log(session);
 
   if (!session) {
     return {
       props: { authenticated: false }
     };
+  } else if (!session.spotifyTokens || !session.spotifyTokens.accessToken) {
+    return {
+      props: {
+        authenticated: true,
+        accessToken: 'none',
+      }
+    }
+  } else if (session.spotifyTokens.expiresAt <= Math.floor(Date.now() / 1000)) {
+    return {
+      props: {
+        authenticated: true,
+        accessToken: 'expired',
+      }
+    }
   } else {
-    console.log(session);
-  }
+    const response = await fetch('https://api.spotify.com/v1/me/playlists', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${session.spotifyTokens.accessToken}`,
+      },
+    });
+    const data = await response.json();
 
-  return { props: { session: session, authenticated: true } };
+    console.log(`GOT DATA`)
+    console.log(Object.keys(data))
+    if (response.ok) {
+      const playlists = data.items.map((item: { id: string, name: string, [ key: string ]: unknown }) => (
+        { id: item.id, name: item.name }
+      ))
+      return { 
+        props: {
+          authenticated: true,
+          accessToken: 'valid',
+          playlists: playlists
+        }
+      }
+    } else {
+      const error = data;
+      console.error(error)
+      return {
+        props: {
+          authenticated: true,
+          accessToken: 'valid',
+          error: error,
+        }
+      }
+    }
+  }
 };
