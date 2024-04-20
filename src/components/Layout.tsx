cebfbfada8b63342ccda4b4616e721f5e7f6240e
Rebecca Peckman <rebeccapeckman@gmail.com>
2024-04-20 01:47:06 -0700

    sidebar component with mock playlist data

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,23 +1,27 @@
 import { Inter } from "next/font/google";
+
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 import { cn } from "@/lib/utils";
-import Header from "@/components/Header";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <main
       className={cn(
-        "h-[100vh] bg-background font-sans antialiased",
+        "flex flex-col h-[100vh] bg-background font-sans antialiased",
         inter.variable
       )}
     >
@@ -27,9 +31,7 @@ export default function Layout({ children }: Readonly<{ children: React.ReactNod
         className="h-full w-full border"
       >
         <ResizablePanel defaultSize={20} minSize={10}>
-          <div className="flex h-full items-center justify-center p-6">
-            <span className="font-semibold">Sidebar</span>
-          </div>
+          <Sidebar />
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={75}>
