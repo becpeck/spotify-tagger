444f2d4f39768d9cb81efbc24652e51f947de112
Rebecca Peckman <rebeccapeckman@gmail.com>
2024-04-22 15:25:21 -0700

    playlistColumns now displays track, artists, album names, formatted time and date

--- src/components/TrackTable/playlistColumns.tsx
+++ src/components/TrackTable/playlistColumns.tsx
@@ -11,29 +11,76 @@ export type Track = {
   duration_ms: number;
 };
 
+function stringifyDuration(ms: number): string {
+  const totalSeconds = Math.floor(ms / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+  const seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+
+  return `${hours ? `${hours}:` : ''}`
+    + `${hours ? String(minutes).padStart(2, '0') : minutes}:`
+    + `${String(seconds).padStart(2, '0')}`;
+}
+
 export const columns: ColumnDef<Track>[] = [
   {
     accessorKey: "number",
-    header: "#"
+    header: () => (<div className="text-right">#</div>),
+    cell: ({ row }) => (
+      <div className="text-right">
+        {row.getValue("number")}
+      </div>
+    ),
   },
   {
     accessorKey: "track",
     header: "Title",
+    cell: ({ row }) => (
+      <div>
+        {(row.getValue("track") satisfies Data).name}
+      </div>
+    ),
   },
   {
     accessorKey: "artists",
     header: "Artist",
+    cell: ({ row }) => (
+      <>
+        {(row.getValue("artists") satisfies Data[]).map(artist => (
+          <div key={artist.id}>{artist.name}</div>
+        ))}
+      </>
+    ),
   },
   {
     accessorKey: "album",
     header: "Album",
+    cell: ({ row }) => (
+      <div>
+        {(row.getValue("album") satisfies Data).name}
+      </div>
+    ),
   },
   {
     accessorKey: "added_at",
     header: "Date Added",
+    cell: ({ row }) => (
+      <div>
+        {(new Date(String(row.getValue("added_at")))).toLocaleDateString("en-US", {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+        })}
+      </div>
+    ),
   },
   {
     accessorKey: "duration_ms",
-    header: "Duration",
+    header: () => (<div className="text-right">Duration</div>),
+    cell: ({ row }) => (
+      <div className="text-right">
+        {stringifyDuration(parseInt(row.getValue("duration_ms")))}
+      </div>
+    ),
   },
 ];
