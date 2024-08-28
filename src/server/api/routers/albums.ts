ddef92d66326f5740b5fbaebd571503c2b3e8268
Rebecca Peckman <rebeccapeckman@gmail.com>
2024-08-28 01:03:50 -0400

    updated getAlbumData trpc route to get isSaved data

--- src/server/api/routers/albums.ts
+++ src/server/api/routers/albums.ts
@@ -1,11 +1,26 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { AlbumIdSchema } from "@/server/spotifyWebApi/utils/schemas";
 
+import { checkSavedTracks } from "@/server/api/routers/tracks";
+
 const albumsRouter = createTRPCRouter({
   getAlbumData: protectedProcedure
     .input(AlbumIdSchema)
-    .query(({ input, ctx }) => {
-      return ctx.spotify.getAlbum({ params: { album_id: input } });
+    .query(async ({ input, ctx }) => {
+      const album = await ctx.spotify.getAlbum({ params: { album_id: input } });
+      const isSaved = await checkSavedTracks(
+        ctx.spotify,
+        album.tracks.items.map((track) => track.id)
+      );
+      return {
+        ...album,
+        tracks: {
+          ...album.tracks.items.map((track, i) => ({
+            ...track,
+            is_saved: isSaved[i]!,
+          })),
+        },
+      };
     }),
 });
 
