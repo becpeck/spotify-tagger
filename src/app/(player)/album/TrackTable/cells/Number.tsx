671b09015805f9d41c80e824ec63561054e7fee5
Rebecca Peckman <rebeccapeckman@gmail.com>
2024-09-01 23:11:08 -0400

    modified number cells to use PlayPauseButton component, better alignment

--- src/app/(player)/album/TrackTable/cells/Number.tsx
+++ src/app/(player)/album/TrackTable/cells/Number.tsx
@@ -1,9 +1,9 @@
 import { type CellContext } from "@tanstack/react-table";
-import { HashIcon, PlayIcon, PauseIcon } from "lucide-react";
+import { HashIcon } from "lucide-react";
 
 import { type AlbumTrack } from "@/app/(player)/album/TrackTable";
-import { type ExtendedCellContext } from "@/app/(player)/album/trackTable/TrackTableRow";
-import { Button } from "@/components/ui/button";
+import { type ExtendedCellContext } from "@/app/(player)/album/TrackTable/TrackTableRow";
+import PlayPauseButton from "@/components/buttons/PlayPauseButton";
 
 import { useAppStore } from "@/lib/stores/AppStoreProvider";
 import { trpc } from "@/lib/trpc/client";
@@ -11,7 +11,7 @@ import { cn } from "@/lib/utils";
 
 export function NumberHeader() {
   return (
-    <div className="w-full flex justify-center align-center">
+    <div className="w-full flex justify-end px-1">
       <span className="sr-only">Track Number</span>
       <HashIcon size={15} />
     </div>
@@ -31,7 +31,6 @@ export function NumberCell(props: CellContext<AlbumTrack, number>) {
   );
   const playContextMutation = trpc.playback.playWithContext.useMutation(); // TODO: add secondary playback state context
   const isPlaying = isPlaybackContext && !playbackState!.paused;
-  const Icon = isPlaying ? PauseIcon : PlayIcon;
 
   const toggleIsPlaying = async () => {
     if (!isPlaybackContext) {
@@ -49,28 +48,24 @@ export function NumberCell(props: CellContext<AlbumTrack, number>) {
   };
 
   return (
-    <Button
-      variant="ghost"
-      className="h-8 w-8 p-0 text-muted-foreground"
-      disabled={!player}
-      onClick={toggleIsPlaying}
-    >
+    <div className="w-full flex justify-end">
+      <PlayPauseButton
+        size="sm"
+        variant="normal"
+        className="hidden group-hover/row:flex text-primary h-8 w-[calc(100% - 0.25rem)]"
+        disabled={!playbackState || !player}
+        isPlaying={isPlaying}
+        onClick={toggleIsPlaying}
+        aria-label={isPlaying ? "Pause" : "Play"}
+      />
       <div
         className={cn(
-          "group-hover/row:hidden tabular-nums text-base",
+          "group-hover/row:hidden tabular-nums text-base text-muted-foreground px-1",
           isPlaybackContext && "text-green-500"
         )}
       >
         {row.original.track_number}
       </div>
-      <div className="hidden group-hover/row:block">
-        <span className="sr-only">{isPlaying ? "Pause" : "Play"}</span>
-        <Icon
-          size={15}
-          fill="hsl(var(--primary))"
-          stroke="hsl(var(--primary))"
-        />
-      </div>
-    </Button>
+    </div>
   );
 }
